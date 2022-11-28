d1a12ad957dd517de52626af2047f3bdd8b3ec11
jsungl <wotjd931026@gmail.com>
2022-11-29 01:59:41 +0900

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
+import {useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/test')
+    .then(res => console.log(res.data));
+  })
   return (
     <div className="App">
       <div>React 웹 페이지</div>
--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+if (process.env.NODE_ENV === 'production') {
+    require("dotenv").config({ path: path.join(__dirname, '.env.prod') });
+} else {
+    //require("dotenv").config(); dotenv 모듈 불러오기
+    require("dotenv").config({ path: path.join(__dirname, '.env.develop') });
+}
+
+const port = process.env.PORT;
+
+app.use(express.json());
+const cors = require('cors');
+app.use(cors());
 
 app.get('/', function(req,res) {
     res.send('hello node.js server')
 });
 
-app.listen(5000, function () {
-    console.log('listening on 5000')
+app.get('/test',function(req,res) {
+    res.send('전송 성공!')
+});
+
+app.listen(port, function () {
+    console.log(`listening on ${port}`);
 });
